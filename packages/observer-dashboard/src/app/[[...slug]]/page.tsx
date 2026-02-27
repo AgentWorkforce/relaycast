6ebe4cc1cd5854709de45bae4d0f96eb565db45d
Khaliq <khaliqgant@gmail.com>
2026-02-27 10:30:10 +0100

    support automatic api key auth with key in query param

--- packages/observer-dashboard/src/app/[[...slug]]/page.tsx
+++ packages/observer-dashboard/src/app/[[...slug]]/page.tsx
@@ -1,3 +1,4 @@
+import { Suspense } from 'react';
 import { RelaySessionProvider } from '../../components/RelaySessionProvider';
 import { DashboardLayout } from '../../components/DashboardLayout';
 
@@ -5,8 +6,10 @@ export const runtime = 'edge';
 
 export default function CatchAllPage() {
   return (
-    <RelaySessionProvider>
-      <DashboardLayout />
-    </RelaySessionProvider>
+    <Suspense fallback={<div className="min-h-screen flex items-center justify-center"><div className="animate-spin h-8 w-8 border-2 border-indigo-500 border-t-transparent rounded-full" /></div>}>
+      <RelaySessionProvider>
+        <DashboardLayout />
+      </RelaySessionProvider>
+    </Suspense>
   );
 }
